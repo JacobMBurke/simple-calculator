d9d52ae02bfec7058d8797e3a73c82ca17560c43
Todgrim <jacobburke@hotmail.co.uk>
2019-05-22 08:33:41 +0100

    Added more unit tests for calculatorEngine

--- SimpleCalculator.Test.Unit/CalculatorEngineTest.cs
+++ SimpleCalculator.Test.Unit/CalculatorEngineTest.cs
@@ -27,5 +27,75 @@ namespace SimpleCalculator.Test.Unit
             Assert.AreEqual(3, result);
 
         }
+
+        [TestMethod]
+        public void SubtractsTwoNumbersAndReturnsValidResultForSymbolOperation()
+        {
+            double number1 = 5;
+            double number2 = 2;
+            double result = _calculatorEngine.Calculate("-", number1, number2);
+            Assert.AreEqual(3, result);
+
+        }
+
+        [TestMethod]
+        public void SubtractsTwoNumbersAndReturnsValidResultForNonSymbolOperation()
+        {
+            double number1 = 5;
+            double number2 = 2;
+            double result = _calculatorEngine.Calculate("subtract", number1, number2);
+            Assert.AreEqual(3, result);
+
+        }
+
+        [TestMethod]
+        public void MultipliesTwoNumbersAndReturnsValidResultForSymbolOperation()
+        {
+            double number1 = 5;
+            double number2 = 2;
+            double result = _calculatorEngine.Calculate("*", number1, number2);
+            Assert.AreEqual(10, result);
+
+        }
+
+        [TestMethod]
+        public void MultipliesTwoNumbersAndReturnsValidResultForNonSymbolOperation()
+        {
+            double number1 = 5;
+            double number2 = 2;
+            double result = _calculatorEngine.Calculate("multiply", number1, number2);
+            Assert.AreEqual(10, result);
+
+        }
+
+        [TestMethod]
+        public void DividesTwoNumbersAndReturnsValidResultForSymbolOperation()
+        {
+            double number1 = 5;
+            double number2 = 2;
+            double result = _calculatorEngine.Calculate("/", number1, number2);
+            Assert.AreEqual(2.5, result);
+
+        }
+
+        [TestMethod]
+        public void DividesTwoNumbersAndReturnsValidResultForNonSymbolOperation()
+        {
+            double number1 = 5;
+            double number2 = 2;
+            double result = _calculatorEngine.Calculate("divide", number1, number2);
+            Assert.AreEqual(2.5, result);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void FailsToProcessUnrecognizedOperation()
+        {
+            double number1 = 1;
+            double number2 = 2;
+            double result = _calculatorEngine.Calculate("unrecognised", number1, number2);
+            
+        }
     }
 }
