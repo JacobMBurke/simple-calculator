49f75638fb18dc37b22fdff322df15b199e5ddce
Todgrim <jacobburke@hotmail.co.uk>
2019-05-22 08:12:37 +0100

    Initial commit

---
+++ SimpleCalculator.Test.Unit/CalculatorEngineTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculator.Test.Unit
+{
+    [TestClass]
+    public class CalculatorEngineTest
+    {
+        private readonly CalculatorEngine _calculatorEngine = new CalculatorEngine();
+
+        [TestMethod]
+        public void AddsTwoNumbersAndReturnsValidResultForNonSymbolOperation()
+        {
+            double number1 = 1;
+            double number2 = 2;
+            double result = _calculatorEngine.Calculate("add", number1, number2);
+            Assert.AreEqual(3, result);
+
+        }
+
+        [TestMethod]
+        public void AddsTwoNumbersAndReturnsValidResultForSymbolOperation()
+        {
+            double number1 = 1;
+            double number2 = 2;
+            double result = _calculatorEngine.Calculate("+", number1, number2);
+            Assert.AreEqual(3, result);
+
+        }
+    }
+}
---
+++ SimpleCalculator/CalculatorEngine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class CalculatorEngine
+    {
+        public double Calculate(string argOperation, double argFirstNum, double argSecondNum)
+        {
+            double result;
+
+            switch (argOperation.ToLower())
+            {
+                case "add":
+                case "+":
+                    result = argFirstNum + argSecondNum;
+                    break;
+                case "subtract":
+                case "-":
+                    result = argFirstNum - argSecondNum;
+                    break;
+                case "multiply":
+                case "*":
+                    result = argFirstNum * argSecondNum;
+                    break;
+                case "divide":
+                case "/":
+                    result = argFirstNum / argSecondNum;
+                    break;
+                default:
+                    throw new InvalidOperationException("Specified operation is not recognised");
+            }
+
+            return result;
+        }
+    }
+}
